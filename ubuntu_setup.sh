44fcd426d0574d608da1948f31877197f10bcef9
Dirk Degel <dirk.degel@gmail.com>
2021-10-24 22:09:46 +1000

    some updates

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -21,10 +21,24 @@ sudo apt purge xiterm+thai -y
 sudo apt purge vim -y
 sudo apt purge byobu -y
 
-sudo apt install neofetch
+sudo apt install htop -y # installed
+sudo apt wget -y # installed
+sudo apt install neofetch -y
+sudo apt install nautilus -y
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
 # sudo apt install r-base-core -y # 4.0.4
 
 ### optinal stuff
 sudo apt install gnome-tweaks -y
+
+https://github.com/vinceliuice/Matcha-gtk-theme.git
+cd Matcha-gtk-theme/
+./install.sh
+
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt install papirus-icon-theme
 
 # reboot
