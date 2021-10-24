e22b7539bd602c7595ac96bd37da2277eb3db149
Dirk Degel <dirk.degel@gmail.com>
2021-10-24 22:53:07 +1000

    added visual studio code

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -26,10 +26,21 @@ sudo apt wget -y # installed
 sudo apt install neofetch -y
 sudo apt install nautilus -y
 sudo apt install gedit -y
+sudo apt install eog -y
 
 # chrome browser
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+# some problems
+
+# visual studio code
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code -y
 
 # sudo apt install r-base-core -y # 4.0.4
 
